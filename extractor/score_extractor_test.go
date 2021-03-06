6aeab9fe61953837d58a32e7279b25f9c610de6c
Kenje Hofileña <kenje4090@gmail.com>
2021-03-07 00:04:32 +0800

    Add tetris test

--- extractor/score_extractor_test.go
+++ extractor/score_extractor_test.go
@@ -5,6 +5,14 @@ import (
 	"testing"
 )
 
+func Reader(t *testing.T, filename string) []byte {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		t.Fatalf("error reading file: %v", err)
+	}
+	return data
+}
+
 func TestScoreExtractor_Pacman(t *testing.T) {
 	tests := []struct {
 		scenario string
@@ -25,11 +33,12 @@ func TestScoreExtractor_Pacman(t *testing.T) {
 	for _, tc := range tests {
 		t.Run(tc.scenario, func(t *testing.T) {
 			got := ExtractPacman(Reader(t, tc.filename))
+
 			if tc.want.Lives != got.Lives {
 				t.Errorf("want lives %v, got %v", tc.want.Lives, got.Lives)
 			}
 			if tc.want.Level != got.Level {
-				t.Errorf("want levels %v, got %v", tc.want.Level, got.Level)
+				t.Errorf("want level %v, got %v", tc.want.Level, got.Level)
 			}
 			if tc.want.Score != got.Score {
 				t.Errorf("want score %v, got %v", tc.want.Score, got.Score)
@@ -38,10 +47,32 @@ func TestScoreExtractor_Pacman(t *testing.T) {
 	}
 }
 
-func Reader(t *testing.T, filename string) []byte {
-	data, err := ioutil.ReadFile(filename)
-	if err != nil {
-		t.Fatalf("error reading file: %v", err)
+func TestScoreExtractor_Tetris(t *testing.T) {
+	tests := []struct {
+		scenario string
+		filename string
+		want     Tetris
+	}{
+		{
+			scenario: "Tetris",
+			filename: "./checkpoints_test_data/1615022713.ram",
+			want: Tetris{
+				Level: 1,
+				Score: 1475,
+			},
+		},
 	}
-	return data
+
+	for _, tc := range tests {
+		t.Run(tc.scenario, func(t *testing.T) {
+			got := ExtractTetris(Reader(t, tc.filename))
+
+			if tc.want.Level != got.Level {
+				t.Errorf("want level %v, got %v", tc.want.Level, got.Level)
+			}
+			if tc.want.Score != got.Score {
+				t.Errorf("want score %v, got %v", tc.want.Score, got.Score)
+			}
+		})
+	}
 }
